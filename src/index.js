b5798279c6186b539081a169f51ea2fc1fa628ea
alenamor7 <morozkinalyona@gmail.com>
2021-08-23 00:17:06 +0300

    feat: implement main logic for morse-decoder

--- src/index.js
+++ src/index.js
@@ -37,8 +37,30 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+const SPACE = '**********';
+const LETTER_LENGTH = 10;
+const SYMBOL_LENGTH = 2;
+const MORSE_NUMS = {
+    '00' : '',
+    '10' : '.',
+    '11' : '-',
+};
+
 function decode(expr) {
-    // write your solution here
+    const exprArr = expr.split(SPACE);
+    const exprArrDecoded = exprArr.map((word) => {
+        let decodedWord = '';
+        for (let i = 0; i < word.length; i+= LETTER_LENGTH) {
+            const symbol = word.substring(i, i + LETTER_LENGTH);
+            let decodedSymbol = ''
+            for (let j = 0; j < symbol.length; j += SYMBOL_LENGTH) {
+                decodedSymbol += MORSE_NUMS[symbol.substring(j, j + SYMBOL_LENGTH)];
+            }
+            decodedWord += MORSE_TABLE[decodedSymbol];
+        }
+        return decodedWord;
+    });
+    return exprArrDecoded.join(' ');
 }
 
 module.exports = {
